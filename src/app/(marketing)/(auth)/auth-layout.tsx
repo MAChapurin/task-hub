11a4e07db9f9d7090e4a2e603f3ad77d39880f1f
MAChapurin <machapurin@icloud.com>
2025-07-11 18:06:13 -0700

    fix error name in auth-layout, add client variant auth-layout

---
+++ src/app/(marketing)/(auth)/auth-layout.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { PATHNAMES } from '@/shared/constants/pathnames';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/shared/ui/dialog';
+import { AuthAnimateTabs } from '@/widgets/auth-animate-tabs';
+import { usePathname, useRouter } from 'next/navigation';
+import { ReactNode } from 'react';
+
+export function AuthClientLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const handleClose = () => {
+    router.replace(PATHNAMES.HOME);
+  };
+
+  const isOpen = pathname === PATHNAMES.LOGIN || pathname === PATHNAMES.REGISTER;
+
+  return (
+    <Dialog open={isOpen} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Вход в систему</DialogTitle>
+        </DialogHeader>
+        <AuthAnimateTabs />
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/app/(marketing)/(auth)/layout.tsx
+++ src/app/(marketing)/(auth)/layout.tsx
@@ -1,32 +1,10 @@
-'use client';
-import { PATHNAMES } from '@/shared/constants/pathnames';
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/shared/ui/dialog';
-// import { AuthAnimateTabs } from '@/widgets/auth-animate-tabs';
-import { usePathname, useRouter } from 'next/navigation';
 import { ReactNode } from 'react';
+import { AuthClientLayout } from './auth-layout';
 
-export default function AuthLAyaout({
+export default async function AuthLayout({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
-  const router = useRouter();
-  const pathname = usePathname();
-  const handleClose = () => {
-    router.replace(PATHNAMES.HOME);
-  };
-
-  const isOpen = pathname === PATHNAMES.LOGIN || pathname === PATHNAMES.REGISTER;
-
-  return (
-    <Dialog open={isOpen} onOpenChange={handleClose}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Вход в систему</DialogTitle>
-        </DialogHeader>
-        {/* <AuthAnimateTabs /> */}
-        {children}
-      </DialogContent>
-    </Dialog>
-  );
+  return <AuthClientLayout>{children}</AuthClientLayout>;
 }
