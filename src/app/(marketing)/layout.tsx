0db739c76773a8ee80b3c155779244865b6ece19
MAChapurin <machapurin@icloud.com>
2025-07-11 18:20:29 -0700

    simplified structure in auth-layout

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -3,6 +3,7 @@ import { AboutSection } from '@/widgets/about-section';
 import { FAQAccordionSection } from '@/widgets/faq-section';
 import { FeaturesSrction } from '@/widgets/features-section';
 import { HeroSection } from '@/widgets/hero/ui/hero';
+import { AuthClientLayout } from './auth-layout';
 
 export default async function MarketingLayout({
   children,
@@ -10,8 +11,8 @@ export default async function MarketingLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      {children}
+    <div>
+      <AuthClientLayout>{children}</AuthClientLayout>
       <HeaderMarketing />
       <main className="min-h-screen w-full max-w-400 flex flex-col items-center justify-center  p-2 md:p-4 mx-auto scroll-smooth">
         <HeroSection />
@@ -22,6 +23,6 @@ export default async function MarketingLayout({
       <footer className="mt-16 text-center text-gray-500 text-sm max-w-xl mx-auto px-4">
         &copy; {new Date().getFullYear()} Taskhub. All rights reserved.
       </footer>
-    </>
+    </div>
   );
 }
