fa803c49f9e4a22fd77788b9965e21fe001ba077
MAChapurin <machapurin@icloud.com>
2025-07-27 01:47:09 -0700

    add icon to status-badge

--- src/widgets/project-list/ui/status-badge.tsx
+++ src/widgets/project-list/ui/status-badge.tsx
@@ -1,4 +1,5 @@
 import { Badge } from '@/shared/ui/badge';
+import { CheckIcon, ClockIcon, LoaderIcon } from 'lucide-react';
 import { FC } from 'react';
 
 type StatusBadgeProps = {
@@ -7,12 +8,18 @@ type StatusBadgeProps = {
 
 export const StatusBadge: FC<StatusBadgeProps> = ({ status }) => {
   const statusMap = {
-    BACKLOG: { label: 'Ожидает', color: 'bg-gray-500' },
-    IN_PROGRESS: { label: 'В работе', color: 'bg-blue-600' },
-    DONE: { label: 'Завершён', color: 'bg-green-600' },
+    BACKLOG: { label: 'Ожидает', color: 'bg-gray-500', icon: ClockIcon },
+    IN_PROGRESS: { label: 'В работе', color: 'bg-blue-600', icon: LoaderIcon },
+    DONE: { label: 'Завершён', color: 'bg-green-600', icon: CheckIcon },
   };
 
   const { label, color } = statusMap[status];
+  const Icon = statusMap[status].icon;
 
-  return <Badge className={`${color} text-white`}>{label}</Badge>;
+  return (
+    <Badge className={`${color} text-white`}>
+      <Icon className="w-3 h-3" />
+      {label}
+    </Badge>
+  );
 };
